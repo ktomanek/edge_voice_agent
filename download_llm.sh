dfc3941bfc651bbe0a16feb920eb666236840dbd
Katrin Tomanek <katrin.tomanek@gmx.de>
2026-05-05 15:35:20 -0700

    use LFM2.5 models

--- download_llm.sh
+++ download_llm.sh
@@ -1,8 +1,6 @@
 # NOTE: this is just one example
 mkdir -p models/llms
 
-wget https://huggingface.co/LiquidAI/LFM2-350M-GGUF/resolve/main/LFM2-350M-Q4_K_M.gguf -O models/llms/LFM2-350M-Q4_K_M.gguf
-wget https://huggingface.co/LiquidAI/LFM2-1.2B-GGUF/resolve/main/LFM2-1.2B-Q4_0.gguf -O models/llms/LFM2-1.2B-Q4_0.gguf
-wget https://huggingface.co/unsloth/gemma-3-1b-it-GGUF/resolve/main/gemma-3-1b-it-Q4_0.gguf -O models/llms/gemma-3-1b-it-Q4_0.gguf
+wget https://huggingface.co/LiquidAI/LFM2.5-350M-GGUF/resolve/main/LFM2.5-350M-Q4_K_M.gguf -O models/llms/LFM2.5-350M-Q4_K_M.gguf
+wget https://huggingface.co/LiquidAI/LFM2.5-1.2B-Instruct-GGUF/resolve/main/LFM2.5-1.2B-Instruct-Q4_K_M.gguf -O models/llms/LFM2.5-1.2B-Instruct-Q4_K_M.gguf
 wget https://huggingface.co/unsloth/gemma-3-1b-it-GGUF/resolve/main/gemma-3-1b-it-Q4_K_M.gguf -O models/llms/gemma-3-1b-it-Q4_K_M.gguf
-
--- start_llama_server_agent.sh
+++ start_llama_server_agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 llama-server \
-    -m /root/dev/edge_voice_agent/models/llms/LFM2-1.2B-Q4_0.gguf \
+    -m /root/dev/edge_voice_agent/models/llms/LFM2.5-1.2B-Instruct-Q4_K_M.gguf \
     --port 8080 > /var/log/llama-server.log 2>&1 &
